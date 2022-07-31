4fbe286c39abeeef260b63278b8a7bf43285dab2
Sebastian Doe <39373619+sebastiandoe5@users.noreply.github.com>
2022-07-31 22:20:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// change this
-const countdownDay = new Date(window.location.hash);
+const hash = window.location.hash;
 
+if (hash === '') window.location.href = '/create';
 
+const countdownDay = new Date(hash);
 const difference = countdownDay.getTime() - new Date().getTime();
 const days = Math.round(difference / 86400000).toString();
 document.getElementById('daysLeft').innerText = days;
